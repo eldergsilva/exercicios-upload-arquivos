5157603e4cc621d48bc6e8a7ea3067b59c4b5b0b
eldergsilva <eldergsilva@gmail.com>
2026-07-16 10:27:12 -0300

    verifica usuario

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
-
+const login = require('./controladores/login');
 const rotas = express();
 
 rotas.post('/usuarios', usuarios.cadastrarUsuario);
+
+rotas.post('/login', login.login);
 
 module.exports = rotas;
