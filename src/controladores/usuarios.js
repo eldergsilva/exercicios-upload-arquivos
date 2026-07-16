55335e09322f001cf26d8551f367143cc8a4cc0c
eldergsilva <eldergsilva@gmail.com>
2026-07-16 10:34:42 -0300

    atualizar perfil

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -53,7 +53,53 @@ const obterPerfil = async (req, res) => {
     return res.status(200).json(req.usuario);
 }
 
+const atualizarPerfil = async (req, res) => {
+    let { nome, email, senha, nome_loja } = req.body;
+    const { id } = req.usuario;
+
+    if (!nome && !email && !senha && !nome_loja) {
+        return res.status(404).json('É obrigatório informar ao menos um campo para atualização');
+    }
+
+    try {
+        const usuarioExiste = await knex('usuarios').where({ id }).first();
+
+        if (!usuarioExiste) {
+            return res.status(404).json('Usuario não encontrado');
+        }
+
+        if (senha) {
+            senha = await bcrypt.hash(senha, 10);
+        }
+
+        if (email !== req.usuario.email) {
+            const emailUsuarioExiste = await knex('usuarios').where({ email }).first();
+
+            if (emailUsuarioExiste) {
+                return res.status(404).json('O Email já existe.');
+            }
+        }
+
+        const usuarioAtualizado = await knex('usuarios')
+            .where({ id })
+            .update({
+                nome,
+                email,
+                senha,
+                nome_loja
+            });
+
+        if (!usuarioAtualizado) {
+            return res.status(400).json("O usuario não foi atualizado");
+        }
+
+        return res.status(200).json('Usuario foi atualizado com sucesso.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
 module.exports = {
     cadastrarUsuario,
-    obterPerfil
+    obterPerfil,
+    atualizarPerfil
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
+const verificaLogin = require('./filtros/verificaLogin');
 const rotas = express();
 
 rotas.post('/usuarios', usuarios.cadastrarUsuario);
 
 rotas.post('/login', login.login);
+
+rotas.use(verificaLogin)
+
+rotas.get('/perfil', usuarios.obterPerfil);
+rotas.put('/perfil', usuarios.atualizarPerfil);
 
 module.exports = rotas;
