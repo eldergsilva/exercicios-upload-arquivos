d374c588f66992a7249874835ae390060932505d
eldergsilva <eldergsilva@gmail.com>
2026-07-17 10:25:07 -0300

    multer

--- src/rotas.js
+++ src/rotas.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const usuarios = require('./controladores/usuarios');
 const login = require('./controladores/login');
+const produtos = require('./controladores/produtos');
 const verificaLogin = require('./filtros/verificaLogin');
+const multer = require('./filtros/multer')
 const rotas = express();
 
+ 
 rotas.post('/usuarios', usuarios.cadastrarUsuario);
 
+ 
 rotas.post('/login', login.login);
-
-rotas.use(verificaLogin)
-
+ 
+rotas.use(verificaLogin);
+ 
 rotas.get('/perfil', usuarios.obterPerfil);
 rotas.put('/perfil', usuarios.atualizarPerfil);
+
+ 
+rotas.get('/produtos', produtos.listarProdutos);
+rotas.get('/produtos/:id', produtos.obterProduto);
+rotas.post('/produtos',multer.single('imagem'), produtos.cadastrarProduto);
+rotas.put('/produtos/:id', produtos.atualizarProduto);
+rotas.delete('/produtos/:id', produtos.excluirProduto);
 
 module.exports = rotas;
